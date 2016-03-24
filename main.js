42615fa31db9cc6b79b45df2c5ba399cb9fc2bad
Manali Nirhali <manali8785@gmail.com>
2016-03-24 11:34:13 -0700

    third commit

--- main.js
+++ main.js
@@ -3,14 +3,40 @@
  */
 
 var app=angular.module("UIChallenge",[]);
+var map;
+function initMap() {
+    console.log("Google Map is getting initialized");
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
 app.controller('controller',function($scope){
+
     $scope.getMyLocation=function(){
+        console.log("getting my location...");
         $.ajax({
             type : 'GET',
             url : 'http://ip-api.com/json/',
             success : function(response){
+                console.log('success function');
                 $scope.data=response;
             }
         });
+    }
+
+    $scope.reset=function(){
+        $scope.data.query="0.0.0.0";
+        $scope.data.country=""
+        $scope.data.regionName="";
+        $scope.data.city="";
+        $scope.data.timezone="";
+        $scope.data.lat="";
+        $scope.data.lon="";
+    }
+
+    $scope.locate=function(){
+        console.log("locating website");
+
     }
 });
