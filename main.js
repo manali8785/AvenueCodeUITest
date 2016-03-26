939332a0d5fe9df90428875e808b7d6aa8a87000
Manali Nirhali <manali8785@gmail.com>
2016-03-25 21:11:48 -0700

    modified

--- main.js
+++ main.js
@@ -7,18 +7,26 @@ var map;
 function init() {
     console.log("Google Map is getting initialized");
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+    //    center: {lat: -34.397, lng: 150.644},
+    //    zoom: 8
     });
 }
 
 app.controller('controller',function($scope,$http,MarkerService){
+    var now = new Date();
+
+    $(".help").click(function(e){
+        var fieldName = $(e.currentTarget).closest('tr').find('.location_value').text();
+        alert("This is your " + fieldName + " from ISP " + $scope.data.isp + " at " + now);
+    });
+
     $scope.getMyLocation=function(){
         console.log("getting my location...");
+        $(".help").css('visibility','visible');
         $http.get("http://ip-api.com/json/").then(function(config){
             $scope.data=config.data;
             var MyLatLng = {lat: config.data.lat, lng: config.data.lon};
-            MarkerService.addMarker(MyLatLng,"My Location",map);
+            MarkerService.addMarker(MyLatLng,"My Location");
         },function(err){
             console.log("Error:",err);
         });
@@ -32,6 +40,7 @@ app.controller('controller',function($scope,$http,MarkerService){
         $scope.data.timezone="";
         $scope.data.lat="";
         $scope.data.lon="";
+        $(".help").css('visibility','hidden');
     }
 
     $scope.locate=function(){
@@ -39,7 +48,7 @@ app.controller('controller',function($scope,$http,MarkerService){
         $http.get("http://ip-api.com/json/"+$scope.website).then(function(config){
             var websiteLatLng = {lat: config.data.lat, lng: config.data.lon};
             console.log(websiteLatLng);
-            MarkerService.addMarker(websiteLatLng,$scope.website,map);
+            MarkerService.addMarker(websiteLatLng,$scope.website);
 
         },function(err){
             console.log("Error:",err);
@@ -49,7 +58,7 @@ app.controller('controller',function($scope,$http,MarkerService){
 });
 
 app.service('MarkerService',function(){
-    this.addMarker=function(latlong,title,map){
+    this.addMarker=function(latlong,title){
         console.log('adding marker',latlong);
         map.setCenter(latlong);
         map.setZoom(10);
